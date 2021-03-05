dcca68112a57b8b270a317cd220a028f85080c13
Abdurrahman-dotcom <79723690+Abdurrahman-dotcom@users.noreply.github.com>
2021-03-05 21:28:32 +0300

    Update Git_GitHubC.java

--- src/Git_GitHubp/Git_GitHubC.java
+++ src/Git_GitHubp/Git_GitHubC.java
@@ -3,8 +3,9 @@ package Git_GitHubp;
 public class Git_GitHubC {
 
 	public static void main(String[] args) {
-	
+		
 		System.out.println("Hello world from Java");
+		System.out.println("Hello world from gitHun");
 	}
 
 }
